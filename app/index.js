68735f8a14d21c624d097ca2e4e60c8c6f8b21ce
unknown <christia@Corp-3197.ideal.corp.local>
2016-03-23 11:45:37 -0400

    fixed es5 syntax blowing up babel

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 import d3 from 'd3';
-//require('./index.css');
+require('./index.css');
 
-var data = [5, 10, 15, 20, 25];
+const data = [5, 10, 15, 20, 25];
 
 d3.select('body').selectAll('div')
     .data(data)
     .enter()
     .append('div')
     .attr('class', 'bar')
-    .style('height', d => { return d * 7 + 'px'};
+    .style('height', d => { return d * 7 + 'px'});
